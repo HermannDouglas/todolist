9c5c304d9adf15810442c657daf335fd6628c7a5
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2024-12-19 15:59:25 -0500

    feat: adicionar ID do usuário na criação de tarefas no TaskController

--- src/main/java/br/com/hermann/todolist/task/TaskController.java
+++ src/main/java/br/com/hermann/todolist/task/TaskController.java
@@ -1,21 +1,27 @@
 package br.com.hermann.todolist.task;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
-    
+
     @Autowired
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel) {
+    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         System.out.println("Chegou no controller");
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
         var task = this.taskRepository.save(taskModel);
         return task;
     }
